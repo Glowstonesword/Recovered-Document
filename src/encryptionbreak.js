29656fc61825cabbc1674310953e9d47f7b5a8a9
Glowstonesword <sam.wadey@hotmail.com>
2025-08-11 20:28:42 +0100

    Create encryptionbreak.js

---
+++ src/encryptionbreak.js
@@ -0,0 +1,46 @@
+var dictionary = "0123456789qwertyuiopasdfghjklzxcvbnm!?></\a`~+*=@#$%".split('');
+
+var lvl1 = document.getElementById('lvl1');
+var lvl2 = document.getElementById('lvl2');
+
+var ran = function() {
+ return Math.floor(Math.random() * dictionary.length)
+}
+
+var ranString = function(amt) {
+  var string = '';
+  for(var i = 0; i < amt; i++) {
+    string += dictionary[ran()];
+  }
+  return string;
+}
+
+var init = function(str) {
+  var count = str.length;
+  var delay = 50;
+  
+  lvl1.innerHTML = '';
+  lvl2.innerHTML = '';
+  
+  var gen = setInterval(function() {
+    lvl1.setAttribute('data-before', ranString(count));
+    lvl1.setAttribute('data-after', ranString(count));
+    lvl2.setAttribute('data-before', ranString(count));
+    lvl2.setAttribute('data-after', ranString(count));
+    if(delay > 0) {
+      delay--;
+    }
+    else {
+      if(count < str.length) {
+        el.innerHTML += str[str.length - count-1];
+      }
+      count--;
+      if(count === -1) {
+        clearInterval(gen);
+        showButton();
+      }
+    }
+  }, 32);
+}
+
+});
