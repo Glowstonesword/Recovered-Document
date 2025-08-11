6d5756c558c15b5b3d42303a91845f7318957655
Glowstonesword <sam.wadey@hotmail.com>
2025-08-11 20:17:10 +0100

    Update entryscript.js

--- src/entryscript.js
+++ src/entryscript.js
@@ -48,6 +48,6 @@ var showButton = function() {
 const content = document.getElementById('Title');
 btn.addEventListener('click', function() {
  content.innerHTML = '<h2>SWAG</h2>';
- btn.innerHTML = ''; });
+ btn.remove(); });
 
 init('File Encrypted');
